93225a027a84b51ffa4faa69ba2cc491c6089885
Askdaddy <askdaddy@gmail.com>
2017-11-02 17:43:15 +0800

    post to pypi

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ from distutils.core import setup
 setup(
     name='ansible-dynamic-inventory-mysql',
     packages=['ansible-dynamic-inventory-mysql'],
-    version='0.0.1',
-    description='ansible-dynamic-inventory-mysql',
-    author='None',
-    author_email='None',
-    maintainer='ansible-dynamic-inventory-mysql',
-    maintainer_email='',
+    package_dir={'': '..'},
+    version='0.1.0',
+    python_requires='>=3',
+    description='This is a Dynamic Inventory for Ansible to be used together with MySQL.',
+    author='Askdaddy',
+    author_email='askdaddy@gmail.com',
     url='https://github.com/askdaddy/ansible-dynamic-inventory-mysql',
     download_url='https://github.com/askdaddy/ansible-dynamic-inventory-mysql/archive/arya.zip',
     install_requires=[
         'configparser>=3.5.0',
         'PyMySQL>=0.7.11'
       ],
+    keywords=['ansible','inventory','mysql']
 )
